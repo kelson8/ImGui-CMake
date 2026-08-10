c34cb93493be33a771ec5a4c57a0a3e954f8a68a
kelson8 <kelson@kelsoncraft.net>
2026-08-09 20:49:57 -0400

    Add Lua test script

---
+++ scripts/test.lua
@@ -0,0 +1,10 @@
+print("Test from LUA code.")
+
+-- Printing values from the C++ code.
+-- print("Value printed in LUA from C++: " .. newtest.foo())
+-- print("Value printed in LUA from C++: " .. newtest.var1)
+-- print("Value printed in LUA from C++: " .. newtest.var2)
+
+-- newtest.var2 = 200.2
+
+-- print("New value for var2: " .. newtest.var2)
